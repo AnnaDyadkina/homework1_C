db1a560c0fb4858ddd6c0f196bf9e9e402f42720
Anna <anna.skorohodova@inbox.ru>
2022-10-02 00:19:20 +0300

    task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int count = 2;
+string result = "";
+if (number == 1) {
+  result = "Нет четных чисел";
+}
+else {
+  while (count <= number) {
+    if (count == 2) {
+      result = result + count;
+    }
+    else {
+        result = result + ", " + count;
+    }
+    count = count + 2;
+  }
+}
+Console.WriteLine(result);
